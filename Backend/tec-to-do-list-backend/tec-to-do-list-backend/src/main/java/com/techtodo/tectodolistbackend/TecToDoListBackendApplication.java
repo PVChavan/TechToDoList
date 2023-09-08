6b1567db71fb3eecd8dbe01e182b92477e957945
PrachiChavan <pvchavan14@gmail.com>
2023-09-08 09:27:37 +0530

    project directory for backend

---
+++ Backend/tec-to-do-list-backend/tec-to-do-list-backend/src/main/java/com/techtodo/tectodolistbackend/TecToDoListBackendApplication.java
@@ -0,0 +1,13 @@
+package com.techtodo.tectodolistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecToDoListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecToDoListBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/tec-to-do-list-backend/tec-to-do-list-backend/src/test/java/com/techtodo/tectodolistbackend/TecToDoListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtodo.tectodolistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecToDoListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
